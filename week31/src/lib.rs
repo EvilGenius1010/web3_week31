e0a28ed707c443e432833d03a06312f40e4e7608
Harshavardhan K <120200220+EvilGenius1010@users.noreply.github.com>
2025-06-23 05:33:15 +0000

    finished code for counter program

--- week31/src/lib.rs
+++ week31/src/lib.rs
@@ -14,31 +14,64 @@ use solana_program::{
 
 entrypoint!(program_counter_contract);
 
+#[derive(BorshSerialize,BorshDeserialize)]
+struct InstructionType{
+    Increment(u32),
+    Decrement(u32)
+}
+
+#[derive(BorshSerialize,BorshDeserialize)]
+struct Counter{
+    count:u32
+}
+
+
 pub fn program_counter_contract(
     account_addr: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    
     msg!("Testing ");
 
     let acc_iter = next_account_info(accounts.iter())?;
 
-    match acc_iter {
-        Ok(account_info) => {
-            msg!(
-                "Correct account taken and its data is {:?}!",
-                account_info.data
-            );
+
+    let instruction_type = InstructionType::try_from_slice(instruction_data)?;
+
+    let mut counter_data = Counter::try_from_slice(&acc_iter.data.borrow())?;
+
+    match instruction_type{
+        InstructionType::Increment(val)=>{
+            msg!("Executing increase {}",val);
+            counter_data.count+=val;
         }
-        Err(e) => return e,
+
+        InstructionType::Decrement(val)=>{
+            msg!("Executing decrease {}",val);
+            counter_data.count-=val;
+        }
+
+
+        counter_data.serialize(&mut *acc_iter.data.borrow_mut());
+
+        msg!("value mutation successful!");
     }
+    
+    // match acc_iter {
+    //     Ok(account_info) => {
+    //         msg!(
+    //             "Correct account taken and its data is {:?}!",
+    //             account_info.data
+    //         );
+    //     }
+    //     Err(e) => return e,
+    // }
 
     Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use solana_program::clock::Epoch;
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use solana_program::clock::Epoch;
+// }
